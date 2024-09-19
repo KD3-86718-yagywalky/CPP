d99cfbf30b73ff74765766138d3d8d15af2ec31a
Yagywalky <yagywalkydubey08@gmail.com>
2024-09-19 17:20:36 +0530

    Assignment07-01

--- Assignment07-01.cpp
+++ Assignment07-01.cpp
@@ -1,154 +1,234 @@
 #include<iostream>
 using namespace std;
 
-class Product
-{
-    public:
-    int id;
+class Product{
+
+    int ID;
     string title;
+
+    public :
     double price;
 
-Product()
-{
-    id=0;
-    title="xyz";
-    price=10;
-}
-Product(int x,string y,int p)
-{
-    this->id=x;
-    this->title=y;
-    this->price=p;
-}
-virtual void accept()
-{
-    cout<<"Enter product Id :";
-    cin>>id;
-    cout<<"Enter product Title :";
-    cin>>title;
-    cout<<"Enter product Price :";
-    cin>>price;
-}
-virtual void display()
-{
-    cout<<"product Id :"<<id<<endl;
-    cout<<"product Title :"<<title<<endl;
-    cout<<"product Price :"<<price<<endl;
-}
+    Product():ID(0),title(" "){}
+
+    Product(int ID, string title,double price):ID(ID),title(title),price(price){ }
+
+    virtual void accept()
+    {
+        cout<<" ID of the product :- ";
+        cin>>ID;
+        cout<<endl;
+
+        cout<<"Title of the product :- ";
+        cin>>title;
+        cout <<endl;
+
+         cout<<"Price of the product :- ";
+        cin>>price;
+        cout <<endl;
+                
+    }
+
+    virtual void display()
+    {
+        cout<<"ID :- "<<ID<<endl;
+        cout<<"title :- "<<title<<endl;
+        cout<<"price :- "<<price<<endl;
+    }
+
+    void getID(int id)
+    {
+        this->ID = id;
+    }
+
+    void getTitle(string title)
+    {
+        this->title = title;
+    }
+
+    int setId()
+    {
+        return ID;
+    }
+
+    string setTilte()
+    {
+        return title;
+    }
+
+    void setPrice(double price)
+    {
+        this->price = price;
+    }
+
+    double getPrice()
+    {
+        return price;
+    }
+
+
 };
 
-class Book :public Product
+class Book : public Product
 {
-    string auther;
-public:
-Book()
-{
-    auther="aaaah";
-}
-Book(int id,string title,double price,string auther):Product(id,title,price)
-{
-   this->auther;
-}
-void accept()
-{
-    cout<<"Enter the detail of Book product :"<<endl;
-    Product::accept();
-    cout<<"Enter Auther name :";
-    cin>>auther;
-}
-double calculatediscountPrice(double price)
-{ 
-    return price-price*.9;
-}
-void display()
-{
-     Product::display();
-     cout<<"Auther name :"<<auther<<endl;
-}
-};
-class Tape:public Product
-{
-    string artist;
+    string author;
 
-public:
-Tape()
-{
-    artist="ZZ";
+    public :
 
-}
-Tape(int id,string title,double price,string artist):Product(id,title,price)
-{
-    this->artist=artist;
-}
-void accept()
-{
-    cout<<"Enter the detail of Tape product :"<<endl;
-    Product::accept();
+    Book():author(" "){}
+
+    Book(int ID , string title, string author, double price): Product(ID,title,price),author(author){}
+
+    void accept()
+    {   
+        Product::accept();
+        this->author = author;
+        cout<<"Enter author name :- ";
+        cin>>author;
+    }
+
+    void display()
+    {
+        Product::display();
+        cout<<" Author :- "<<author<<endl;
+        cout <<" Real price of the Book :- "<<price<<endl;
     
-    cout<<"Enter Artist name :";
-    cin>>artist;
-}
 
-void display()
-{
-    cout<<"Display the detail of Book product :"<<endl;
-     Product::display();
-     cout<<"Artist name :"<<artist<<endl;
-}
+    }
+
+    
 };
 
-int main()
-{
-
-    Product * arr[3];
-    int choice;
-    for(int i = 0; i<3;i++)
-    { 
-        cout<<"0. for buy Book"<<endl;
-        cout<<"1. for buy tape"<<endl;
-        cin>>choice;
-        if(choice==0)
-        {
-        arr[i]=new Book();
-        arr[i]->accept();
-        }
-        else if(choice==1)
-        {
-            arr[i]=new Tape();
-            arr[i]->accept();
-        }
-        else
-        {
-            cout<<"wrong choice......."<<endl;
-        }
-    }
-
-    for(int i=0;i<3;i++)
+class tapes: public Product
     {
-        arr[i]->display();
-    }
+        string artist;
 
-    double total=0;
-    double totaldiscount=0;
-    double x;
-    for(int i=0;i<3;i++)
-    {
-        if(typeid(arr[i])==typeid(Book))
+        public:
+
+        tapes():artist(""){}
+
+        tapes(int ID, string title, string artist, double price)
         {
-        x= arr[i]->price;
-        total+=x;
-        totaldiscount+=x*.90;
-        }
-        else{
-        x= arr[i]->price;
-        total+=x;
-        totaldiscount+=x*.95;
+            this->getID(ID);
+            this->getTitle(title);
+            this->artist=artist;
+            this->price = price;
         }
 
+        void accept()
+        {
+            Product::accept();
+            cout<<" Enter name of the artist :- ";
+            cin>>artist;
+            cout<<endl;
+
+        }
+
+        void display()
+        {
+            Product::display();
+            cout<<"Artist :- "<<artist<<endl;
+            cout<<" Actual price of tape :- "<<price<<endl;
+
+        }
+        
+    };
+
+    int menu(int choice)
+    {
+          cout << "*******************" << endl;
+        cout<<"0. For not buying anything "<<endl;
+        cout<<"1. For buy Books "<<endl;
+        cout<<"2. For buy tape "<<endl;
+        cout<<"3.display all purchase order bill "<<endl;
+        cout<<"Enter your choice :- ";
+        cin>>choice ;
+        return choice;
+          cout << "*******************" << endl;
     }
-    cout<<"Total Price :"<<total<<endl;
-    cout<<"Discounted Price :"<<totaldiscount;
-    return 0;
 
-}
-  
+    int main()
+    {   
+        int many;
+        cout<<"How much product you want to buy :- ";
+        cin>>many;
+        Product *arr[many];
+        int index=0;
+        
+        int choice ;
+        while((choice = menu(choice))!=0)
+        {
+            switch(choice)
+            {
+                case 1: 
+                if(index<many)
+                {
+                     arr[index] = new Book;
+                     arr[index]->accept();
+                    index++;
+                }
+                else {
+                    cout << "Array is full .....";
+                }
+                break;
+
+                case 2:
+                         if(index<many)
+                         {
+                             arr[index]=new tapes;
+                             arr[index]->accept();
+                             index++;
+                         }
+                         else {
+                             cout<<"Array is full.....";
+                         }
+
+                        break;
+
+
+                
+                case 3:
+                    {
+
+                        double Total = 0;
+
+                        for(int i=0; i<index;i++){
+                        if(typeid(*arr[i])==typeid(Book)){
+
+                            arr[i]->display();
+                            Total = Total + (arr[i]->getPrice()*0.90);
+                        }
+
+                        else if(typeid(*arr[i])==typeid(tapes))
+                        {   
+                            arr[i]->display();
+                            Total = Total + (arr[i]->getPrice()*0.95);
+                        }
+                        else{
+                            cout<<endl;
+                        }
+                        }
+
+                        cout<<"Total amount :- "<<Total<<endl;
+                    }
+                       break;
+                
+                default :
+                       cout<<"Wrong Input ......";
+                       break;
+
+        }
+        }
+
+        for(int i=0; i<index; i++)
+        {
+            delete arr[i];
+            arr[i]=NULL;
+        }
+
+        return 0;
+    }   
+
+
+
+    
